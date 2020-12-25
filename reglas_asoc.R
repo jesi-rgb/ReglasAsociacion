74bdc290c6ce875db0064bc53af11983165b2941
Jesús Enrique Rascón <blograso@gmail.com>
2020-12-25 14:02:50 +0100

    we got transactions now

--- reglas_asoc.R
+++ reglas_asoc.R
@@ -4,14 +4,25 @@ library(arules)
 groceries = read.csv("Groceries_dataset.csv")
 head(groceries)
 
-
-groceries$Date <- as.Date(groceries$Date, "%d-%m-%Y")
-groceries$itemDescription <- as.factor(groceries$itemDescription)
+groceries$Date = as.Date(groceries$Date, "%d-%m-%Y")
+groceries$itemDescription = as.factor(groceries$itemDescription)
 groceries$Member_number <- as.factor(groceries$Member_number)
 
-categories <- subset(groceries$itemDescription)
+str(groceries)
+
+groupings <- groceries %>% group_by(Member_number, Date) %>% group_split()
+head(groupings)
+
+take_items = function (tib){
+  a = tib[,3][[1]]
+  return(a)
+}
+
+trans_itemsets = lapply(groupings, take_items)
+  # we should give an index to every transaction in order to continue
+names(trans_itemsets) = 1:length(trans_itemsets)
+head(trans_itemsets)
 
 
-transactions <- groceries %>% group_by(Member_number, Date) %>% group_split()
-head(transactions)
-class(transactions)
+transactions = as(trans_itemsets, "transactions")
+
